85b55d527e66fcf8ddb509b7eea3f57f0ecc6a1a
tanisha <solankitanisha403@gmail.com>
2025-11-22 16:56:43 +0000

    finshed code

--- key_schedule.v
+++ key_schedule.v
@@ -11,5 +11,18 @@ module key_schedule( output wire [ 55 : 0 ] r,
                       input wire [  3 : 0 ] i );
 
   // Stage 1: complete this module implementation
+  wire [27:0]r1;
+  wire [27:0]r0;
+  wire [27:0]clr_l;
+  wire [27:0]clr_r;
+  wire [55:0]merged;
+  
+  split_0 t0(.x(x), .r1(r1), .r0(r0));
+  clr_28bit t1(.x(r1),.r(clr_l),.y(i));
+  clr_28bit t2(.x(r0),.r(clr_r),.y(i));
+  merge_0 t3(.x1(clr_l),.x0(clr_r),.r(merged));
+  assign r = merged;
+  perm_PC2 t4(.x(merged), .r(k));
+  
 
 endmodule
